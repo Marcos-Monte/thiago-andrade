ff1b440fd8f7de34379f18ebeaecba9080c0cb5d
Burunga <monte_marcos@hotmail.com>
2024-06-10 01:02:32 -0300

    Inversão da ordem de componentes dentro do Form

--- src/pages/components/Form/Form.jsx
+++ src/pages/components/Form/Form.jsx
@@ -8,24 +8,22 @@ function Proposta(){
             <legend className={styles.legend}>Dados da sua proposta</legend>
             
             <div className={styles.option}>
-                <label htmlFor="id-titulo">Titulo</label>
+                <label className={styles.label} htmlFor="id-titulo">Titulo</label>
                 <input name='title' id='id-titulo' type="text" placeholder="Digite o titulo da sua proposta"/>
             </div>
 
             <div className={styles.option}>
-            <label htmlFor="id-tema">Tema</label>
+            <label className={styles.label} htmlFor="id-tema">Tema</label>
                 <select name="themes" id="id-tema" required>
                     <option selected value="">Escolha o tema aqui --</option>
                     <option value="instituto">Instituto Federal na ZN</option>
-                    <option value="instituto">Instituto Federal na ZN</option>
-                    <option value="instituto">Instituto Federal na ZN</option>
-                    <option value="instituto">Instituto Federal na ZN</option>
-                    <option value="instituto">Instituto Federal na ZN</option>
+                    <option value="instituto">Reforma Trabalhista</option>
+                    <option value="instituto">Juventude Santista</option>
                 </select>
             </div>
 
             <div className={styles.option}>
-                <label htmlFor="id-proposta">Proposta</label>
+                <label className={styles.label} htmlFor="id-proposta">Proposta</label>
                 <textarea className={styles.textArea} name="proposta" id="id-proposta" placeholder="Digite aqui a sua ideia para o plano de governo"></textarea>
             </div>
 
@@ -37,33 +35,33 @@ function Data(){
     return(
         <fieldset className={styles.group}>
 
-                            <legend className={styles.legend}>Dados pessoais</legend>
+            <legend className={styles.legend}>Dados pessoais</legend>
 
-                            <div className={styles.option}>
-                                <label htmlFor="id-name">Nome</label>
-                                <input name='name' id='id-name' type="text" placeholder="Digite seu nome completo aqui"/>
-                            </div>
+            <div className={styles.option}>
+                <label className={styles.label} htmlFor="id-name">Nome</label>
+                <input name='name' id='id-name' type="text" placeholder="Digite seu nome completo aqui"/>
+            </div>
 
-                            <div className={styles.option}>
-                                <label htmlFor="id-email">E-mail</label>
-                                <input name='email' id='id-email' type="email" placeholder="exemplo@exemplo.com"/>
-                            </div>
+            <div className={styles.option}>
+                <label className={styles.label} htmlFor="id-email">E-mail</label>
+                <input name='email' id='id-email' type="email" placeholder="exemplo@exemplo.com"/>
+            </div>
 
-                            <div className={styles.subGroup}>
-                                
-                                <div className={styles.option}>
-                                    <label htmlFor="id-tel">Telefone</label>
-                                    <input name='phone' id='id-tel' type="tel"placeholder="(xx)xxxxx-xxxx" />
-                                </div>
-                                
-                                <div className={styles.option}>
-                                    <label htmlFor="id-cep">CEP</label>
-                                    <input name='cep' id='id-cep' type="tel"placeholder="000000-000" />
-                                </div>
+            <div className={styles.subGroup}>
+                
+                <div className={styles.option}>
+                    <label className={styles.label} htmlFor="id-tel">Telefone</label>
+                    <input name='phone' id='id-tel' type="tel"placeholder="(xx)xxxxx-xxxx" />
+                </div>
+                
+                <div className={styles.option}>
+                    <label className={styles.label} htmlFor="id-cep">CEP</label>
+                    <input name='cep' id='id-cep' type="tel"placeholder="000000-000" />
+                </div>
 
-                            </div>
+            </div>
 
-                        </fieldset>
+        </fieldset>
     )
 }
 
@@ -78,9 +76,9 @@ export default function Form(){
 
             <form className={styles.form} action="" method="post">
 
-                <Proposta />
-
                 <Data />
+                
+                <Proposta />
 
                 <ButtonForm style='buttonForm' />
 
