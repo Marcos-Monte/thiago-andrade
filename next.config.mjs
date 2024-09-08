acf099043eaac81d1252237fdcb7939f74b670a1
Burunga <monte_marcos@hotmail.com>
2024-09-08 12:17:00 -0300

    style: ajuste na resolução do componente Home

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output:'export',
-  images:{
-    unoptimized: true,
-  },
-  basePath: '', // Deixe vazio se o site estiver na raiz, ou adicione o caminho, por exemplo '/meusite'
-  assetPrefix: './', // Isso ajuda com caminhos relativos
-  trailingSlash: true,
 };
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output:'export',
+//   images:{
+//     unoptimized: true,
+//   },
+//   basePath: '', // Deixe vazio se o site estiver na raiz, ou adicione o caminho, por exemplo '/meusite'
+//   assetPrefix: './', // Isso ajuda com caminhos relativos
+//   trailingSlash: true,
+// };
 
 export default nextConfig;
