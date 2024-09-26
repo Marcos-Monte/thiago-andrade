55ada2197a983746e63c30d9929e61297cce873a
Burunga <monte_marcos@hotmail.com>
2024-09-26 20:45:11 -0300

    Atualizando o description

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,11 +8,13 @@ export default function App() {
 
   const title = 'Thiago Andrade'
 
+    const descricao = 'Thiago Andrade, candidato a vereador em Santos-SP, apresenta seu compromisso com a defesa da democracia, justiça social e igualdade de oportunidades. Com foco em educação de qualidade, saúde acessível e empregos dignos, Thiago luta por uma cidade mais justa para todos os santistas. Conheça suas propostas e faça parte da mudança para um futuro melhor'
+
   return (
     <>
       <Head>
         <title>{`${title} | 65123`}</title>
-        <meta name="description" content="Feed do Candidato a Vereador da cidade de Santos, Thiago Andrade" />      
+        <meta name="description" content={descricao} />      
       </Head>
 
       <div className={`body`}>
